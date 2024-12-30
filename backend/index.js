2ca3459b652091ace09893184cef7198b80dad45
BarunGosain <barun.22.010@gmail.com>
2024-12-30 11:54:40 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin:['https://to-do-api-gamma.vercel.app'],
+    origin:['https://to-do-track-pi.vercel.app'],
     methods:["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
