0a3f8d0f3daeb403aafae62ab61e6862d6e4013a
BarunGosain <barun.22.010@gmail.com>
2024-12-30 12:29:41 +0530

    change monogoDB connection settings

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,16 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://barungosain:Bg21182114@cluster0.joiqc.mongodb.net/todolistDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch(err => {
+//     console.error('Error connecting to MongoDB', err);
+// });
+
+mongoose.connect("mongodb+srv://barungosain:Bg21182114@cluster0.joiqc.mongodb.net/todolistDB").then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => {
     console.error('Error connecting to MongoDB', err);
